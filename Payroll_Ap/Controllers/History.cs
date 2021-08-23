004d8a9a1ceed18379f8119dc4d66dd9b83e3fa5
Ephrem Demlew <ephyaabb@gmail.com>
2021-08-23 06:45:00 +0300

    Add project files.

---
+++ Payroll_Ap/Controllers/History.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payroll_Ap.Controllers
+{
+    public class History : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Payroll_Ap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Payroll_Ap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
